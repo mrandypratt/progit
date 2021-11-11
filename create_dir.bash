1deb7b061a2b782eea487f3571038c2874d9db73
mrandypratt <mrandypratt@gmail.com>
2021-11-11 19:58:40 +0000

    Added files for Presentation on .gitignore and git diff

---
+++ create_dir.bash
@@ -0,0 +1,14 @@
+mkdir git_practice && cd git_practice
+mkdir dir1
+mkdir dir1/dir2
+mkdir dir1/dir2/dir3
+echo "Some Text" >> root-file.txt
+echo "Some JavaScript" >> root-file.js
+echo "Some Text" >> dir1/file1.txt
+echo "Some JavaScript" >> dir1/file1.js
+echo "Some Text" >> dir1/dir2/file2.txt
+echo "Some JavaScript" >> dir1/dir2/file2.js
+echo "Some Text" >> dir1/dir2/dir3/file3.txt
+echo "Some JavaScript" >> dir1/dir2/dir3/file3.js
+git init
+
